29b6683f8969b331f27b075fb20df51cfb1ee484
Cesar Murillo <namiayumis@gmail.com>
2020-02-04 22:24:01 +0900

    creando función cuando se da submit

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -19,10 +19,19 @@ const Formulario = () => {
   // Extraer los valores
   const { mascota, propietario, fecha, hora, sintomas } = cita;
 
+  // Cuando el usuario presiona agregar cita
+  const submitCita = e => {
+    e.preventDefault();
+
+    // Validar
+
+    // Asignar un ID
+  };
+
   return (
     <>
       <h2>Crear cita</h2>
-      <form>
+      <form onSubmit={submitCita}>
         <label>Nombre de la mascota</label>
         <input
           type="text"
@@ -49,15 +58,6 @@ const Formulario = () => {
           onChange={actualizarState}
           value={fecha}
         />
-
-        <label>Fecha</label>
-        <input
-          type="date"
-          name="fecha"
-          className="u-full-width"
-          onChange={actualizarState}
-          value={fecha}
-        />
         <label>Hora</label>
         <input
           type="time"
@@ -71,6 +71,7 @@ const Formulario = () => {
           className="u-full-width"
           name="sintomas"
           onChange={actualizarState}
+          value={sintomas}
         ></textarea>
         <button type="submit" className="u-full-width button-primary">
           Agregar Cita
