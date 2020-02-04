6c769493b148036cb49c7d56363152e98ebddaba
Cesar Murillo <namiayumis@gmail.com>
2020-02-04 16:45:29 +0900

    formulario de nueva cita

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Formulario = () => {
-  return <div>Desde formulario</div>;
+  return (
+    <>
+      <h2>Crear cita</h2>
+      <form>
+        <label>Nombre de la mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+        />
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Dueño de la mascota"
+        />
+        <label>Fecha</label>
+        <input type="date" name="fecha" className="u-full-width" />
+        <label>Hora</label>
+        <input type="time" name="hora" className="u-full-width" />
+        <label>Fecha</label>
+        <input type="date" name="fecha" className="u-full-width" />
+        <label>Síntomas</label>
+        <textarea className="u-full-width" name="sintomas"></textarea>
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Formulario;
