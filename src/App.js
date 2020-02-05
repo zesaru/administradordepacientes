f0b844ab18a3ee253270494a4471365af2a64825
Cesar Murillo <namiayumis@gmail.com>
2020-02-05 22:10:13 +0900

    mensaje condicional

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ function App() {
     const nuevasCitas = citas.filter(cita => cita.id !== id);
     guardarCitas(nuevasCitas);
   };
+
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus Citas";
+
   return (
     <>
       <h1>Administracion de Pacientes</h1>
@@ -25,7 +29,7 @@ function App() {
             <Formulario crearCita={crearCita} />
           </div>
           <div className="one-half column">
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {citas.map(cita => (
               <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
             ))}
